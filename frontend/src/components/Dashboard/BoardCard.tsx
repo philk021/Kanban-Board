8f159cd92ddc26dc54abce2dbf404d969293b9a2
philk021 <194525942+philk021@users.noreply.github.com>
2026-08-31 00:16:55 -0400

    Update UI

--- frontend/src/components/Dashboard/BoardCard.tsx
+++ frontend/src/components/Dashboard/BoardCard.tsx
@@ -4,9 +4,9 @@ function BoardCard({title, role} : {title: string, role: string}) {
   return (
     <div className="board-card">
       {title && role ? 
-      <div>
-        <h1>{title}</h1>
-        <h2>{role}</h2>
+      <div className="board-card-info">
+        <h1 className="board-card-title">{title}</h1>
+        <h2 className="board-card-role">{role}</h2>
       </div> : 
       <h1>Untitled</h1>}
     </div>
